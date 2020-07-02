f1aeae9adcf415a39b41d4a33646239212d124c2
Thomas Juster <juster.thomas@gmail.com>
2020-07-02 20:01:13 +0200

    aye, forgot some stuff

---
+++ dist/precache-manifest.ff1f2f9c55ab682ac25ec9f6ea4b66eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4340795d8bb514746839",
+    "url": "/web-rtc-demo-app/dist/css/app.72a3a56c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/web-rtc-demo-app/dist/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e231c824051b0c74a13065d06684cbf7",
+    "url": "/web-rtc-demo-app/dist/index.html"
+  },
+  {
+    "revision": "4340795d8bb514746839",
+    "url": "/web-rtc-demo-app/dist/js/app.9d23736b.js"
+  },
+  {
+    "revision": "3be131f87c18dd2bb3a6",
+    "url": "/web-rtc-demo-app/dist/js/chunk-vendors.55293dec.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/web-rtc-demo-app/dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/web-rtc-demo-app/dist/robots.txt"
+  }
+]);
